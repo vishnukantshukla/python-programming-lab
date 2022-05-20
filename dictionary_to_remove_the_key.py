2791f94c05d58f089ae8dfbf12784f876f6dbe4a
vishnukantshukla <92075105+vishnukantshukla@users.noreply.github.com>
2022-05-20 21:03:21 +0530

    Add files via upload

---
+++ dictionary_to_remove_the_key.py
@@ -0,0 +1,11 @@
+# Delete the entry of the dictionary using del keyword
+
+dct={'Name':"Vishnukant shukla",'Roll number ':56,"College":"Gla university"}
+del dct['Name']
+print(dct)
+
+#   Delete the entry of the dictionary using pop method 
+dct={'Name':"Vishnukant shukla",'Roll number ':56,"College":"Gla university"}
+dct.pop('Name')
+print(dct)
+
