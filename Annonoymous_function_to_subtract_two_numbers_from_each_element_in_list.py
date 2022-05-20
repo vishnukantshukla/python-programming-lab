f68bb1b7c5867f0c06d86a7f107a2199686f412c
vishnukantshukla <92075105+vishnukantshukla@users.noreply.github.com>
2022-05-20 21:05:29 +0530

    Add files via upload

---
+++ Annonoymous_function_to_subtract_two_numbers_from_each_element_in_list.py
@@ -0,0 +1,11 @@
+# def apna_fun(X):
+#     lst1=[]
+#     for i in X:
+#       lst1.append(i-2)
+#     return lst1
+# lst=[2,3,4,5,6]
+# out=apna_fun(lst)
+# print(out)
+lst=[2,3,4,5,6,7]
+apna_fun=lambda X:X-2
+print(apna_fun)
