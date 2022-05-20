7d5fab02fce7818e7a06bde84b1ce5a54e4d3d52
vishnukantshukla <92075105+vishnukantshukla@users.noreply.github.com>
2022-05-20 21:01:41 +0530

    Add files via upload

---
+++ Dictionary_to_get_maximum_value_in_dictionary.py
@@ -0,0 +1,15 @@
+# lst=input("Enter the list : ")
+# lst1=list(map(str,lst.split()))
+# lst2=int(input("Enter the length of the list of values : "))
+# lst3=[]
+# print("Enter the elements : ")
+# for i in range(0,lst2):
+#     element=int(input())
+#     lst3.append(element)
+# out=dict(zip(lst1,lst3))
+# #out1=out.get(max(out,key=out.get))
+# #out1=max(out.values())
+# out1=max(out.values())
+# print("The maximum value is :",out1)
+
+
