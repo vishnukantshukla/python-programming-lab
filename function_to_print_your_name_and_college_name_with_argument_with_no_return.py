8676657c89fa0567a7a3d6a7d65a38a183dbd6ee
vishnukantshukla <92075105+vishnukantshukla@users.noreply.github.com>
2022-05-21 21:02:06 +0530

    Add files via upload

---
+++ function_to_print_your_name_and_college_name_with_argument_with_no_return.py
@@ -0,0 +1,8 @@
+def info(x):
+    out=f'''
+         my Name is {x}
+         i am presuing B.Tech in GLA University in the department of computer science in GLA university
+         '''
+    return out
+a=info('Vishnukant')
+print(a)
